3d2ac0911e0dfed1c119cac4e06d51bafec31d95
harapeko-wolf <32303847+harapeko-wolf@users.noreply.github.com>
2019-02-08 14:00:49 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app"><img width="25%" src="./assets/logo.png" /> <Tax /></div>
+</template>
+
+<script>
+import Tax from "./components/Tax";
+
+export default {
+  name: "App",
+  components: {
+    Tax
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Tax.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <p>base price: <input type="text" v-model="basePrice" /></p>
+    <p>include price: <input type="text" v-model="includePrice" /></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Tax",
+  data() {
+    return {
+      basePrice: 100
+    };
+  },
+  computed: {
+    includePrice: {
+      get: function() {
+        return parseInt(this.basePrice * 1.08);
+      },
+      set: function(includePrice) {
+        return (this.basePrice = Math.ceil(includePrice / 1.08));
+      }
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
